b4edabe8a7ed97ba1130a6a122be4422014f2f0f
srinaudo <s.rinaudo@contur.it>
2022-04-29 17:05:43 +0200

    Init

---
+++ README.md
@@ -0,0 +1,9 @@
+## Async request demo vanillaJS
+
+Consiglio: accedi da serverino php
+
+php -S localhost:8000
+
+quinfi chiama da browser http://localhost:8000/index.html
+
+Se ti torna utile è gradito un po di credito o un caffè :)
---
+++ app.js
@@ -0,0 +1,58 @@
+class App 
+{
+    constructor() {
+
+    }
+
+    readRegioni() {
+        return fetch('http://localhost:8000/regioni.php', {
+            method: 'POST'
+        }).then(res => {
+            return res.json()
+        })
+    }
+
+    readProvince() {
+        return fetch('http://localhost:8000/province.php', {
+            method: 'POST'
+        }).then(res => {
+            return res.json()
+        })
+    }
+
+    bindData(regioni, province) {
+        return regioni.reduce((reducer, regione) => {
+            
+            reducer[regione.prov_regione] = province.reduce((reducerProvince, provincia, index) => {
+                if(provincia.prov_reg == regione.prov_regione) {
+                    // Ottimizzazione: rimuoviamo l'elemento dalle province
+                    province.splice(index, 1)
+                    reducerProvince.push(provincia)
+                }
+                
+                return reducerProvince
+            }, [])
+
+            return reducer
+        }, [])
+    }
+
+    async bootstrap() {
+        console.log('Bootstrapped!')
+        
+        try {
+            const regioni = await this.readRegioni()
+            console.log(regioni)
+            const province = await this.readProvince()
+            console.log(province)
+            const data = this.bindData(regioni, province)
+            console.log(data)
+        } catch (err) {
+            console.log('Error: ' + err.message);
+        }
+    }
+}
+
+
+let app = new App();
+app.bootstrap();
